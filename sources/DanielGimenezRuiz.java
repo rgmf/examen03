96b737eabce4893bc771a6011ca4bbc1c3d6e520
Daniel Giménez Ruiz <gimenezruizdaniel12@gmail.com>
2025-12-09 17:43:56 +0100

    Creacion de la carpeta sources y java en formato PascalCase

---
+++ sources/DanielGimenezRuiz.java
@@ -0,0 +1,14 @@
+public class DanielGimenezRuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+
+
+        // TODO code application logic here
+    }
+
+}
